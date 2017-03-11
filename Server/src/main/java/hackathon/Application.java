4d92a6b127524bc615125cff919b7e73348b4787
axk580 <axk580@cs.bham.ac.uk>
2017-03-11 19:53:32 +0000

    Updating application settings

--- Server/src/main/java/hackathon/Application.java
+++ Server/src/main/java/hackathon/Application.java
@@ -1,13 +1,21 @@
 package hackathon;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Created by boyanbonev on 11/03/2017.
  */
 
 @SpringBootApplication
+@EntityScan({"hackathon.Entities"})
+@EnableJpaRepositories({"hackathon.Repositories"})
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,  HibernateJpaAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
--- Server/src/main/java/hackathon/Repositories/TicketDao.java
+++ Server/src/main/java/hackathon/Repositories/TicketDao.java
@@ -2,6 +2,7 @@ package hackathon.Repositories;
 
 import hackathon.Entities.Ticket;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
@@ -9,6 +10,7 @@ import javax.transaction.Transactional;
  * Created by boyanbonev on 11/03/2017.
  */
 @Transactional
+@Repository
 public interface TicketDao extends CrudRepository<Ticket, Long> {
     Ticket findById(long id);
 }
