a1d7c807b22b495d2b7ce50dd7c785c15c6abf73
Boyan Bonev <boyan.i.bonev@gmail.com>
2017-03-11 15:18:01 +0000

    Rebuild Spring framework and added all libs.

---
+++ Server/src/Controllers/HeartbeatController.java
@@ -0,0 +1,12 @@
+package Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by boyanbonev on 11/03/2017.
+ */
+@RestController
+public class HeartbeatController {
+}
