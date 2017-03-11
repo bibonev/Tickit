80848f35f0516459992b70b13bac43b01ec7e157
“Anna <anna.banasik@kcl.ac.uk>
2017-03-11 16:12:24 +0000

    Slider height set and material ui working

--- React/FBHack/app/app.js
+++ React/FBHack/app/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// import {AppBar} from 'react-native-material-ui';
 import {
   AppRegistry,
   Text,
@@ -8,9 +9,7 @@ import {
 } from 'react-native';
 
 import Swiper from 'react-native-swiper';
-
-const Dimensions = require('Dimensions');
-const window = Dimensions.get('window');
+import { COLOR, ThemeProvider, Button} from 'react-native-material-ui';
 
 export default class FBHack extends Component {
 
@@ -20,34 +19,32 @@ export default class FBHack extends Component {
 
     return (
 
+      <ThemeProvider>
         <View>
-      <Swiper style={styles.wrapper} scrollEnabled={true} 
+        
+        <Swiper height={600} style={styles.wrapper} scrollEnabled={true} 
 
-      prevButton={
-        <View style={styles.buttonPosition}>
-          <Text style={styles.buttonText}/>
-        </View>}
 
-      nextButton={
-        <View style={styles.buttonPosition}>
-          <Text style={styles.buttonText}>></Text>
+        showsButtons={true} loop={false} showsPagination={true}> 
+
+          <View style={styles.slide1}>
+            <Text style={styles.text}>Log in screen 1</Text>
+          </View>
+          <View style={styles.slide2}>
+            <Text style={styles.text}>Log in screen 2</Text>
+          </View>
+          <View style={styles.slide3}>
+            <Text style={styles.text}>Log in screen 3</Text>
+          </View>
+
+        </Swiper>
+
+          <View>
+            <Button primary text="Log IN"/>
+          </View>
         </View>
-        }
-      showsButtons={true} loop={false} showsPagination={true}> 
 
-        <View style={styles.slide1}>
-          <Text style={styles.text}>Log in screen 1</Text>
-        </View>
-        <View style={styles.slide2}>
-          <Text style={styles.text}>Log in screen 2</Text>
-        </View>
-        <View style={styles.slide3}>
-          <Text style={styles.text}>Log in screen 3</Text>
-        </View>
-
-      </Swiper>
-
-      </View>
+      </ThemeProvider>
 
     );
   }
@@ -56,24 +53,15 @@ export default class FBHack extends Component {
 
 var styles = StyleSheet.create({
   wrapper: {
-  },
-  buttonPosition:{
-    // top:window.height/2-60, 
-    // right: window.width/2-60 
-  },
-  buttonText:{
-    // color:'white',
-    // backgroundColor:'black',
-    // padding:3,
-    // paddingLeft:30,
-    // paddingRight:40
 
   },
+
   slide1: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#9DD6EB',
+
   },
   slide2: {
     flex: 1,
